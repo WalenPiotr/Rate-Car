9df58d60eca966e6fa896bfb960dca5da13b9b2e
Piotr Walen <walen.piotr@gmail.com>
2018-02-22 15:04:59 +0100

    Add brands route Change button text in landing page

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.get("/", function (req, res) {
     res.render("landing.ejs");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.get("/search", function (req, res) {
+    res.send("brands page");
+});
+
+app.listen(process.env.PORT, process.env.IP, function () {
     console.log("Server has started on: ");
     console.log("PORT: " + process.env.PORT);
     console.log("IP: " + process.env.IP);
