6902b9774a61dbd6820f1c0224da1fbcf8f10b2c
Piotr Walen <walen.piotr@gmail.com>
2018-02-22 21:19:05 +0100

    Add new.ejs template Setup get /brands/new and post /brands routes

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var express = require("express")
+var express = require("express");
+var mongoose = require("mongoose");
+
 
 var app = express()
 app.use(express.static(__dirname + "/public"));
+
+var brandSchema = new mongoose.Schema({
+    name: String,
+});
+
 
 app.get("/", function (req, res) {
     res.render("landing.ejs");
@@ -11,10 +18,20 @@ app.get("/brands", function (req, res) {
     res.render("brands.ejs");
 });
 
+app.get("/brands/new", function(req, res){
+    res.render("new.ejs");
+})
+
+app.post("/brands",function(req,res){
+    res.send("post route");
+});
+
+
+
+
 app.get("/models", function (req, res) {
     res.render("models.ejs");
 });
-
 
 app.listen(process.env.PORT, process.env.IP, function () {
     console.log("Server has started on: ");
