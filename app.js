508cfe673f30aed88caefb5235ae5ffa6eac8c79
Piotr Walen <walen.piotr@gmail.com>
2018-02-22 12:30:25 +0100

    Add basic root route

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-require("express")
+var express = require("express")
+
+var app = express()
 
 app.get("/", function (req, res) {
-    res.render("Hello World");
+    res.send("Hello World");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started on: ");
+    console.log("PORT: " + process.env.PORT);
+    console.log("IP: " + process.env.IP);
 });
