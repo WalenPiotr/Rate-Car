1a9c887b0bdf61333d7b1780ea86f35655e0aac7
Piotr Walen <walen.piotr@gmail.com>
2018-02-28 15:32:06 +0100

    Change description in cars page Change user model

--- models/user.js
+++ models/user.js
@@ -2,12 +2,13 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    admin: {type: Boolean, default: false}
+    username: {type: String, unique: true, required: true},
+    password: {type:String, required: true},
+    isAdmin: {type: Boolean, default: false},
+    email: {type: String, unique: true, required: true},
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 }); 
 UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User",UserSchema);
-
- 
