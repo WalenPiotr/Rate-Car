7e4a5ed0b08fcc9556acff5a1d64341d62f5a6b7
Piotr Walen <walen.piotr@gmail.com>
2018-03-01 13:19:50 +0100

    Reformat User schema

--- models/user.js
+++ models/user.js
@@ -2,15 +2,15 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: {type: String, unique: true, required: true},
-    password: {type:String},
-    isAdmin: {type: Boolean, default: false},
-    email: {type: String, unique: true, required: true},
-    isActivated: {type: Boolean, default: false},
+    username: { type: String, unique: true, required: true },
+    password: { type: String },
+    isAdmin: { type: Boolean, default: false },
+    email: { type: String, unique: true, required: true },
+    isActivated: { type: Boolean, default: false },
     activationToken: String,
     resetPasswordToken: String,
     resetPasswordExpires: Date
-}); 
+});
 
 UserSchema.plugin(passportLocalMongoose);
 
